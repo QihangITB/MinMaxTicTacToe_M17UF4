5ab478cf68692653a8b551f96a2a3cce84e002a2
QihangITB <qihang.li.7e7@itb.cat>
2025-02-18 19:56:32 +0100

    add new TurnMove Class

---
+++ MinMaxTicTacToe_Qihang/Assets/Scripts/PlayerAI.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.VisualScripting;
+
+public static class PlayerAI
+{
+    private const int MinDepth = 1;
+    private const int UserToken = 1, AIToken = -1, Empty = 0;
+
+    public static int[,] GetNextPosition()
+    {
+        int[,] position = new int[MinDepth, MinDepth];
+
+
+
+
+        return position;
+    }
+
+    public static void MinMaxWithPruning(int[,] matrix)
+    {
+        int? posX = null;
+        int? posY = null;
+
+        float? value = null;
+        float alpha = float.MinValue;
+        float beta = float.MaxValue;
+
+        List<int[,]> matrixList = new List<int[,]>();
+
+
+    }
+
+    public static int[,] CreateMinMaxTree(int[,] matrix, int user, int ai)
+    {
+        Dictionary<int[,], List<int[,]>> matrixValues = new Dictionary<int[,], List<int[,]>>();
+        List<int[,]> matrixList = new List<int[,]>();
+
+        while (!IsMatrixFull(matrix))
+        {
+            matrixList.Add(Turn(matrix, AIToken));
+        }
+
+        return matrix;
+    }
+
+    // Coje el primer espacio que encuentre y coloca el token en caso de jugador o IA
+    public static int[,] Turn(int[,] matrix, int token)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0 ; j < matrix.GetLength(1); j++)
+            {
+                if (matrix[i,j] == Empty)
+                {
+                    matrix[i, j] = token;
+                    return matrix;
+                }
+            }
+        }
+        return matrix;
+    }
+
+    public static bool IsMatrixFull(int[,] matrix)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                if (matrix[i, j] == Empty)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }   
+}
+
+
+
---
+++ MinMaxTicTacToe_Qihang/Assets/Scripts/TurnMove.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.VisualScripting;
+
+public class TurnMove
+{
+    private const int Empty = 0;
+
+    private int[,] previousMatrix;
+    private int[,] currentMatrix;
+    private List<TurnMove> followingMatrixs = new List<TurnMove>();
+
+    public int[,] PreviousMatrix { get => previousMatrix; set => previousMatrix = value; }
+    public int[,] CurrentMatrix { get => currentMatrix; set => currentMatrix = value; }
+    public List<TurnMove> FollowingMatrixs { get => followingMatrixs; set => followingMatrixs = value; }
+
+    public TurnMove(int[,] previous, int[,] current, int token)
+    {
+        previousMatrix = previous;
+        currentMatrix = current;
+        followingMatrixs = CreateFollowingMovements(currentMatrix, -token);
+    }
+
+    private List<TurnMove> CreateFollowingMovements(int[,] matrix, int token)
+    {
+        List<TurnMove> result = new List<TurnMove>();
+        int[,] process = matrix;
+        int? row, column;
+
+        do
+        {
+            // Obtenemos la siguiente posición
+            (row, column) = NextTurnPosition(process);
+
+            if (row != null && column != null)
+            {
+                // Creamos una nueva matriz con el movimiento simulado y lo guardamos
+                int[,] newMatrix = matrix;
+                newMatrix[(int)row, (int)column] = token;
+                result.Add(new TurnMove(matrix, newMatrix, token));
+
+                // Guardamos también el recorrido para no repetir ("bloquear" la casilla)
+                process[(int)row, (int)column] = token;
+            }
+        } while (row != null && column != null);
+        
+        return result;
+    }
+
+    public static (int?,int?) NextTurnPosition(int[,] matrix)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                if (matrix[i, j] == Empty)
+                {
+
+                    return (i,j);
+                }
+            }
+        }
+        return (null, null);
+    }
+}
+
